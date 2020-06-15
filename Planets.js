2925c93318b69ac60c3407363bcfa68c15fa72e1
harsh-4820 <harsh.beis.15@acharya.ac.in>
2020-06-15 16:44:58 +0530

    1 file modified

--- Planets.js
+++ Planets.js
@@ -1,14 +1,30 @@
 import React from 'react';
-
 class Planet extends React.Component{
   constructor(props){
     super(props);
+    this.state={
+      planetData: []
+    }
+  }
+  componentDidMount(){
+    fetch('https://findfalcone.herokuapp.com/planets')
+    .then(res => res.json())
+    .then(data=>{
+      this.setState({
+        planetData:data
+      })
+    })
   }
   render(){
+    console.log(this.state)
     return (
       <form action="submit">
         <label>Destination1 : </label><br/>
-        <select id = "destination-1" name="destination-1"></select>
+        <select id = "destination-1" name="destination-1">
+        {this.state.planetData.map(obj => {
+          return <option>{obj.name}</option>
+        })}
+        </select>
       </form>
     );
   }
